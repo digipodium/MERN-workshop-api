1ce7709b2f804eb4a66e512b613784469adc0d6a
m-triple-m <triplem656@gmail.com>
2024-09-06 15:41:20 +0530

    added startup file

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./routers/userRouter');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(express.json());
+
+app.use('/user', userRouter);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the workshop demo');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
