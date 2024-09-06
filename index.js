0c2728050dda983c40b3d1036b2588b99c29a69b
m-triple-m <triplem656@gmail.com>
2024-09-06 16:19:22 +0530

    configured env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const userRouter = require('./routers/userRouter');
 const cors = require('cors');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
